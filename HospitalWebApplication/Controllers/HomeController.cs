09ee5877821d740ab2ec029a2d73d003b312f7c8
naveenmishra21 <naveenkumarmishra20191@gmail.com>
2025-05-08 01:02:33 +0530

    changes

--- HospitalWebApplication/Controllers/HomeController.cs
+++ HospitalWebApplication/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using HospitalWebApplication.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing.Constraints;
 using static System.Reflection.Metadata.BlobBuilder;
@@ -22,7 +26,8 @@ namespace HospitalWebApplication.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var data = _dataDbContext.Appointments.ToList();
+            return View(data);
         }
 
         public IActionResult Privacy()
@@ -45,12 +50,19 @@ namespace HospitalWebApplication.Controllers
           
           
         }
+
+        public IActionResult Logout()
+        {
+             HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+             return View();
+        }
+
         [HttpGet]
         public IActionResult Resigter()
         {
             return View();
         }
-
+        [HttpPost]
         public async Task<IActionResult> CreateUser(User user)
         {
             var data = _dataDbContext.Users.FirstOrDefault(x => x.Email == user.Email);
@@ -75,9 +87,20 @@ namespace HospitalWebApplication.Controllers
         }
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        
         public IActionResult Error()
         {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        [Authorize]
+
+        public IActionResult SecurePage()
+        {
+            ViewBag.Name = HttpContext.User.Identity.Name;
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
